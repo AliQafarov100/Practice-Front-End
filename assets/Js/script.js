74f3821b10763923f6be80b10a6af10a4d98962f
Ali <tu8cvbxnx@code.edu.az>
2022-06-15 02:36:47 +0400

    Initial commit

---
+++ assets/Js/script.js
@@ -0,0 +1,6 @@
+let menu = document.querySelector(".menu");
+let response = document.querySelector(".responsive-menu")
+
+menu.addEventListener("click",function(){
+     response.style.visibility = "visible";
+})
